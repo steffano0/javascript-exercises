c209edc7ce0354457361d197b5fa2df8c504a8d3
steffano0 <steffanouromero@gmail.com>
2023-02-28 16:30:35 -0500

    Solve the exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return "ERROR"
+    } else if ( number1 < 0 || number2 < 0) {
+        return "ERROR"
+    }
+    let min;
+    let max;
+    if (number1 > number2) {
+        min = number2;
+        max = number1;
+    } else {
+        min = number1;
+        max = number2;
+    }
+    let totalSum = 0;
+    for (let i = min; i<=max; i++) {
+        totalSum += i
+    }
+    return totalSum
 };
 
 // Do not edit below this line
