50b6497984fbba5191ac305f67916a8d1cc3fecf
steffano0 <steffanouromero@gmail.com>
2023-02-28 15:45:56 -0500

    Solve the exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    
+    for (let i = 1; i < arguments.length; i++) {
+
+        if (array.includes(arguments[i])) {
+            const index = array.indexOf(arguments[i]);
+            array.splice(index,1);
+        } else {
+            continue
+        }
+    }
+    return array
 
 };
 
