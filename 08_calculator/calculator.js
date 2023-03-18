188eaaf178b747724f5ad9077d524ac8a70f7325
steffano0 <steffanouromero@gmail.com>
2023-03-18 18:52:36 -0500

    Solve and test calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b
 	
 };
 
-const sum = function() {
+const sum = function(a) {
+	return a.reduce((s, i) => s + i , 0)
+};
+
+const multiply = function(a) {
+  return a.reduce ((m, i) => m * i,1)
+};
+
+const power = function(a, b) {
+  return a ** b
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n > 1) {
+    return n * factorial(n-1)
+  } else {
+    return 1
+  }
 };
 
 // Do not edit below this line
