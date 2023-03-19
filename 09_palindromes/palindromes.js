4fb9956ed626f2be2713738c7e991a283a3ecf95
steffano0 <steffanouromero@gmail.com>
2023-03-18 19:22:21 -0500

    Solve and test the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let left = 0;
+    let right = string.length - 1
+    const lower = string.toLowerCase();
 
+    while (left <= right) {
+        while (!lower.charAt(left).match(/^[a-zA-Z0-9]+$/)) {
+            left++;
+        }
+        while (!lower.charAt(right).match(/^[a-zA-Z0-9]+$/)) {
+            right--;
+        }
+        if (lower.charAt(left) === lower.charAt(right)) {
+            left++;
+            right--;
+        } else {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
