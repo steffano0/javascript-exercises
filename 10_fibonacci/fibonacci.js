a84df9ab7cb655fb4e8c367ef3c09b6ff067f78b
steffano0 <steffanouromero@gmail.com>
2023-03-18 19:49:57 -0500

    Solve and test the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }   
+        let arr = []
+
+        for (let i = 0; i < n; i++) {
+            if (i === 0 || i === 1) {
+                arr.push(1);
+            } else {
+                arr.push((arr[i-1] + arr[i-2]));
+            }
+        }
+        return arr[arr.length - 1];
+    
+
 
 };
 
