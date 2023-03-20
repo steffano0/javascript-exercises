32fe001cbe48d5c5e3d33674359bc0faa9611cf1
steffano0 <steffanouromero@gmail.com>
2023-03-20 01:01:56 -0500

    Solve and test the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    
+    for (i in arr) {
+        if (!arr[i].yearOfDeath) {
+            arr[i]["yearOfDeath"] = new Date().getFullYear();
+        }
+    }
+    const sortedArr = arr.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+
+    return sortedArr[0];
 
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
